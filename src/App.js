56c79cfd54ea7cea69ff352fc4cafe971595e02c
petuniahtf <priscilliaweber6@hotmail.com>
2022-01-24 14:52:35 +0100

    Test de compo sur un branch diférente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Petunia from './Components/Thierry/petunia';
 import ThierryComponent from './Components/Thierry/Thierry';
 
 function App() {
   return (
     <div className="App">
       <ThierryComponent says='Hello,'/>
+      <Petunia/>
     </div>
   );
 }
